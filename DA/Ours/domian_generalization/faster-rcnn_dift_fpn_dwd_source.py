5fba475d78177125391658e27b9c66a36cf986b6
hby <heboyong@qq.com>
2024-02-28 09:55:10 -0500

    20240228 update

---
+++ DA/Ours/domian_generalization/faster-rcnn_dift_fpn_dwd_source.py
@@ -0,0 +1,10 @@
+_base_ = [
+    '../../_base_/models/faster-rcnn_dift_fpn.py',
+    '../../_base_/da_setting/da_20k_0.1backbone.py',
+    '../../_base_/datasets/domain_generalization/dwd.py'
+]
+
+detector = _base_.model
+detector.roi_head.bbox_head.num_classes = 8
+
+train_cfg = dict(val_interval=10000)
