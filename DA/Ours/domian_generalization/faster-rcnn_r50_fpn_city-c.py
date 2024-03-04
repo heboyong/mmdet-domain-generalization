a4f9c0b9f51c83923400b2a8f053e8b1a1250642
heboyong <heboyong@qq.com>
2024-03-05 00:10:57 +0800

    20240305 update

--- DA/Ours/domian_generalization/faster-rcnn_r50_fpn_city-c.py
+++ DA/Ours/domian_generalization/faster-rcnn_r50_fpn_city-c.py
@@ -1,8 +1,9 @@
 _base_ = [
-    '../../_base_/models/faster-rcnn_r101_fpn.py',
+    '../../_base_/models/faster-rcnn_r50_fpn.py',
     '../../_base_/da_setting/da_80k_0.1backbone.py',
     '../../_base_/datasets/domain_generalization/cityscapes-c.py'
 ]
 
 detector = _base_.model
 detector.roi_head.bbox_head.num_classes = 8
+
