f9f1b66f45364a7b5b42d3e17e1c9918a903e32e
heboyong <heboyong@qq.com>
2024-03-05 21:53:14 +0800

    20240305 update

--- DA/Ours/domian_generalization/faster-rcnn_r101_fpn_foggy_source.py
+++
@@ -1,8 +0,0 @@
-_base_ = [
-    '../../_base_/models/faster-rcnn_r101_fpn.py',
-    '../../_base_/da_setting/da_20k_0.1backbone.py',
-    '../../_base_/datasets/domain_generalization/foggy_cityscapes.py'
-]
-
-detector = _base_.model
-detector.roi_head.bbox_head.num_classes = 8
