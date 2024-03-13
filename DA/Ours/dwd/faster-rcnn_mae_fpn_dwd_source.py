b661f545f48eb69dbcc5285c3eff583a8487a79d
heboyong <heboyong@qq.com>
2024-03-14 02:48:16 +0800

    20240314 update

--- DA/Ours/dwd/faster-rcnn_mae_fpn_dwd_source.py
+++ DA/Ours/dwd/faster-rcnn_mae_fpn_dwd_source.py
@@ -6,5 +6,6 @@ _base_ = [
 
 detector = _base_.model
 detector.roi_head.bbox_head.num_classes = 8
+load_from = 'https://download.openmmlab.com/mmdetection/v3.0/vitdet/vitdet_mask-rcnn_vit-b-mae_lsj-100e/vitdet_mask-rcnn_vit-b-mae_lsj-100e_20230328_153519-e15fe294.pth'
 
 train_cfg = dict(val_interval=10000)
