ca5174a87b054c037807e4753ad12db3d433f9a0
heboyong <heboyong@qq.com>
2024-03-14 02:37:12 +0800

    20240314 update

--- DA/Ours/sim10k_to_city/faster-rcnn_dift_fpn_sim10k_to_city_source.py
+++
@@ -1,8 +0,0 @@
-_base_ = [
-    '../../_base_/models/faster-rcnn_dift_fpn.py',
-    '../../_base_/da_setting/da_20k_0.1backbone.py',
-    '../../_base_/datasets/sim10k_to_city/sim10k_to_city_source.py'
-]
-
-detector = _base_.model
-detector.roi_head.bbox_head.num_classes = 1
--- DA/Ours/veis_to_bdd100k/faster-rcnn_dift_fpn_veis_to_bdd100k_target.py
+++
@@ -1,9 +0,0 @@
-_base_ = [
-    '../../_base_/models/faster-rcnn_dift_fpn.py',
-    '../../_base_/da_setting/da_20k_0.1backbone.py',
-    '../../_base_/datasets/veis_to_bdd100k/veis_to_bdd100k_target.py'
-]
-
-detector = _base_.model
-detector.roi_head.bbox_head.num_classes = 7
-train_cfg = dict(val_interval=4000)
